48fb2c83bd77512f24c2be4d9def67c3ed7e4814
Justin <jmoua1@student.sierracollege.edu>
2020-11-02 20:25:33 -0800

    added deduct() test method

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -19,7 +19,9 @@ public class GiftCardTest
 		
 		assertEquals("getIssuingStore()",
 		issuingStore, card.getIssuingStore());
+		
 	}
+	
 	
 	@Test
 	public void getBalance()
@@ -33,5 +35,22 @@ public class GiftCardTest
 		card = new GiftCard(issuingStore, balance);
 		
 		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test
+	public void deduct()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String result;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		result = "Remaining Balance: " + String.format("%6.2f",
+                Math.abs(balance-50));
+		
+		assertEquals("getBalance()", result, card.deduct(50));
 	}
 }
